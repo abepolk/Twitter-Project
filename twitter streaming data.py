cc37c8613aa6d6140acdde4ac6e16f34f5fe483d
Abraham Polk <abepolk@gmail.com>
2017-07-24 12:06:43 -0400

    commented out debugfunction

--- twitter streaming data.py
+++ twitter streaming data.py
@@ -53,7 +53,7 @@ class TwitterStream:
         self.conn.setopt(pycurl.HTTPHEADER, ['Host: stream.twitter.com',
                                              'Authorization: %s' % self.get_oauth_header()])
         self.conn.setopt(pycurl.LOCALPORT, int(os.environ.get('PORT')))
-        self.conn.setopt(pycurl.DEBUGFUNCTION, self.collect_debug_info)
+        #self.conn.setopt(pycurl.DEBUGFUNCTION, self.collect_debug_info)
         # self.handle_tweet is the method that are called when new tweets arrive
         #self.conn.setopt(pycurl.WRITEFUNCTION, self.handle_tweet)
         
